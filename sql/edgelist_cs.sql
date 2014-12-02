52161bead16c4893052fc4349d932c6550369671
Al Johri <al.johri@gmail.com>
2014-12-02 15:07:55 -0600

    stuff

--- sql/edgelist_cs.sql
+++ sql/edgelist_cs.sql
@@ -2,7 +2,7 @@ WITH group_users AS (
 	SELECT fu.uid, fu.name FROM facebook_users fu
 	JOIN facebook_groups_users gu ON fu.uid = gu.uid
 	JOIN facebook_groups fg ON gu.group_id = fg.group_id
-	WHERE fg.name ILIKE '%Sigma Chi%')
+	WHERE fg.name ILIKE '%computer%')
 SELECT uid1 AS source, uid2 AS target
 	FROM facebook_friends ff
 	LEFT OUTER JOIN facebook_users fu1 ON ff.uid1=fu1.uid
